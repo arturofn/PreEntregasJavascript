44ea770168756c3909cef9cee2aa989ab303de89
Arturo Fernandez <arturofn@outlook.com>
2024-05-22 00:45:42 -0500

    segundo commit

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 
 let montoPrestamo = prompt("ingresa el monto que deseas solicitar");
 let numeroCuotas = prompt("ingresa el numero de cuotas que deseas pagar");
-const montoPorCuota = montoPrestamo/numeroCuotas;
+let montoPorCuota = montoPrestamo/numeroCuotas;
 
 if (numeroCuotas <= 5) {
-
+    montoPorCuota = montoPorCuota + montoPorCuota * 0.05;
+} else {
+    montoPorCuota = montoPorCuota + montoPorCuota * 0.10;
 }
 
 console.log(montoPorCuota);
