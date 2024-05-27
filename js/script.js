2c946bd51f2bb346947b2f6f6d63d026f76790d1
Arturo Fernandez <arturofn@outlook.com>
2024-05-27 15:27:46 -0500

    3er commit

--- js/script.js
+++ js/script.js
@@ -2,19 +2,27 @@
 
 let montoPrestamo = prompt("ingresa el monto que deseas solicitar");
 let numeroCuotas = prompt("ingresa el numero de cuotas que deseas pagar");
-let montoPorCuota = montoPrestamo/numeroCuotas;
 
-if (numeroCuotas <= 5) {
-    montoPorCuota = montoPorCuota + montoPorCuota * 0.05;
-} else {
-    montoPorCuota = montoPorCuota + montoPorCuota * 0.10;
+let miCuota = (montoPrestamo, numeroCuotas) => {
+    let montoPorCuota = montoPrestamo/numeroCuotas;
+    const interes1 = 0.05;
+    const interes2 = 0.10;
+
+    if (numeroCuotas <= 5) {
+        montoPorCuota = montoPorCuota + montoPorCuota * interes1;
+    } else {
+        montoPorCuota = montoPorCuota + montoPorCuota * interes2;
+    }
+
+    return montoPorCuota;
 }
 
-console.log(montoPorCuota);
-alert("El monto por cuota seria: " + montoPorCuota);
+let resultado = miCuota(montoPrestamo, numeroCuotas);
+
+alert("El monto por cuota seria: " + resultado);
 
 // Imprimiendo el monto y numero de orden de cada cuota
 
 for (let numeroDeCuota=1; numeroDeCuota<=numeroCuotas; numeroDeCuota++) {
-    console.log(numeroDeCuota);
+    alert("Cuota Nro" + " " + numeroDeCuota + " " + "es de" + " " + resultado);
 }
