88722155fd4677a92065257604680442ad6e586a
Arturo Fernandez <arturofn@outlook.com>
2024-06-20 23:22:44 -0500

    commit 3era entrega

--- js/script.js
+++ js/script.js
@@ -1,28 +1,81 @@
-// Simulador de Préstamos
+const productos = [
+    {id: 1, nombre: "audifonos", precio: 100},
+    {id: 2, nombre: "parlante", precio: 200},
+    {id: 3, nombre: "reloj", precio: 300},
+]
 
-let montoPrestamo = prompt("ingresa el monto que deseas solicitar");
-let numeroCuotas = prompt("ingresa el numero de cuotas que deseas pagar");
+let carrito = localCarritoFromLocalStorage ();
 
-let miCuota = (montoPrestamo, numeroCuotas) => {
-    let montoPorCuota = montoPrestamo/numeroCuotas;
-    const interes1 = 0.05;
-    const interes2 = 0.10;
+function agregarCarrito (productoId, cantidad) {
+    const producto = productos.find(item => item.id === productoId);
 
-    if (numeroCuotas <= 5) {
-        montoPorCuota = montoPorCuota + montoPorCuota * interes1;
+    const sumarProducto = carrito.find(prod => prod.id === productoId);
+    if(sumarProducto){
+        sumarProducto.cantidad += cantidad;
+        sumarProducto.subTotal = sumarProducto.precio * sumarProducto.cantidad;
     } else {
-        montoPorCuota = montoPorCuota + montoPorCuota * interes2;
+        carrito.push(
+            {
+                id: productoId,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                cantidad: cantidad,
+                subTotal: producto.precio * cantidad
+            }
+        )
     }
 
-    return montoPorCuota;
+    saveCartToLocalStorage();
+    renderCart();
+
 }
 
-let resultado = miCuota(montoPrestamo, numeroCuotas);
-
-alert("El monto por cuota seria: " + resultado);
-
-// Imprimiendo el monto y numero de orden de cada cuota
-
-for (let numeroDeCuota=1; numeroDeCuota<=numeroCuotas; numeroDeCuota++) {
-    alert("Cuota Nro" + " " + numeroDeCuota + " " + "es de" + " " + resultado);
+function renderProducts() {
+    const listaProductos = document.querySelector("#lista-de-productos");
+    listaProductos.innerHTML = " ";
+    productos.forEach(itemp => {
+        const productoDiv = document.createElement("div");
+        productoDiv.innerHTML = `
+                <p>${itemp.nombre} - ${itemp.precio}</p>
+                <button onclick="agregarCarrito(${itemp.id},1)">Agregar al carrito</button
+                `;
+                listaProductos.appendChild(productoDiv);
+    })
 }
+
+function renderCart() {
+    const carritoDiv = document.querySelector("#carrito");
+    carritoDiv.innerHTML = " ";
+    carrito.forEach (p => {
+        const carritoItemDiv = document.createElement("div");
+        carritoItemDiv.innerHTML = `
+                <p>Nombre: ${p.nombre}, Cantidad: ${p.cantidad}, Precio Total: S/${p.subTotal}</p>
+                `;
+                carritoDiv.appendChild(carritoItemDiv);
+    })
+}
+
+function saveCartToLocalStorage() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function localCarritoFromLocalStorage () {
+    const carritoData = localStorage.getItem("carrito");
+    return carritoData ? JSON.parse(carritoData) : [];
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderProducts();
+    renderCart();
+});
+
+
+const vaciarCarrito = document.querySelector("#vaciar");
+const vaciarBtn = document.createElement("button");
+function carritoVacio () {
+    carrito.innerHTML = " ";
+    localStorage.clear();
+}
+vaciarBtn.addEventListener('click', carritoVacio);
+vaciarBtn.innerHTML = "Vaciar carrito";
+vaciarCarrito.appendChild(vaciarBtn);
